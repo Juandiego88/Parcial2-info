2d429137516d31bbe27d08cc92e2e43542917f65
Juandiego88 <62150440+Juandiego88@users.noreply.github.com>
2021-09-28 19:38:23 -0500

    Versión final totalmente estable.

--- CODIGO QT/Parcial2-qt/main.cpp
+++ CODIGO QT/Parcial2-qt/main.cpp
@@ -5,12 +5,14 @@ using namespace std;
 
 int main()
 {
+
     //Imagen a("../Parcial2-qt/imagenes/japon.jpg");
     //Imagen a("../Parcial2-qt/imagenes/col.jpg");
     //Imagen a("../Parcial2-qt/imagenes/antioquia.jpg");
-    Imagen a("../Parcial2-qt/imagenes/islas.jpg");
-    //Imagen a("../Parcial2-qt/imagenes/bandera-alemania.jpg");
 
+    //Imagen a("../Parcial2-qt/imagenes/bandera-alemania.jpg");
+    //Imagen a("../Parcial2-qt/imagenes/brasil.jpg");
+    Imagen a("../Parcial2-qt/imagenes/inglaterra.jpg");
     a.analisis();
     a.mostrar();
     return 0;
